59b25ba6eeba96981151d21424a5400d7d7f0b46
hemanthsamaga <hemanthsamaga@gmail.com>
2024-09-08 14:36:24 +0530

    Question 4

--- Question3.js
+++ Question3.js
@@ -12,3 +12,14 @@ basketHeading.style.color='green';
 
 const thanks=document.getElementById('thanks');
 thanks.innerHTML='<p>Please visit us again</p>';
+
+const fruit = document.getElementsByClassName('fruit');
+console.log(fruit[1]);
+fruit[1].textContent='Pineapple'; 
+fruit[1].style.fontWeight='bold';
+
+for (let i=0; i<fruit.length; i++)
+{
+    fruit[i].style.backgroundColor='gray';
+}
+fruit[2].style.color='red';
