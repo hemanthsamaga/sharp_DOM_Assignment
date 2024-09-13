e08504894b3466c2825e8297904a5ec85ede3287
hemanthsamaga <hemanthsamaga@gmail.com>
2024-09-13 16:56:28 +0530

    Question 10

--- question5.js
+++
@@ -1,8 +0,0 @@
-// Write your code below:
-const listItems=document.getElementsByTagName('li');
-listItems[4].style.color='blue';
-
-for (let i=0; i<listItems.length; i++)
-  {
-    listItems[i].style.fontStyle='italic';
-  }
