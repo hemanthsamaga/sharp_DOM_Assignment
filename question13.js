b9337f3d8c3546a6f797c02afb447aceba86d3b2
hemanthsamaga <mshemanthms12@gmail.com>
2024-10-03 10:00:50 +0530

    Question 13 js

---
+++ question13.js
@@ -0,0 +1,20 @@
+// Write your code below:
+function  handleFormSubmit(event){
+    event.preventDefault();
+  
+    const username = event.target.username.value;
+    const email = event.target.email.value;
+    const phone = event.target.phone.value;
+  
+    const userDetails = {
+      username: username,
+      email: email,
+      phone: phone
+    };
+  
+  localStorage.setItem('User Details', JSON.stringify(userDetails));  
+   
+  }
+  
+  document.onsubmit = handleFormSubmit;
+  module.exports=handleFormSubmit; 
